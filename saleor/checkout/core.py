devneal134/ceiala

Update codebase to use updated prices libraries for money handling

saleor/checkout/core.py
@@ -7,7 +7,7 @@ from django.db import transaction
 from django.forms.models import model_to_dict
 from django.utils.encoding import smart_text
 from django.utils.translation import get_language
-from prices import FixedDiscount, Price
+from prices import Money, TaxedMoney
 
 from ..account.models import Address
 from ..account.utils import store_user_address
@@ -108,7 +108,9 @@ class Checkout:
             if self.shipping_method and partition.is_shipping_required():
                 shipping_cost = self.shipping_method.get_total()
             else:
-                shipping_cost = Price(0, currency=settings.DEFAULT_CURRENCY)
+                shipping_cost = TaxedMoney(
+                    net=Money(0, currency=settings.DEFAULT_CURRENCY),
+                    gross=Money(0, currency=settings.DEFAULT_CURRENCY))
             total_with_shipping = partition.get_total(
                 discounts=self.cart.discounts) + shipping_cost
 
@@ -210,32 +212,41 @@ class Checkout:
         self.modified = True
 
     @property
-    def discount(self):
-        """Return a discount if any."""
+    def discount_amount(self):
+        """Return a discount amount if any."""
         value = self.storage.get('discount_value')
         currency = self.storage.get('discount_currency')
-        name = self.storage.get('discount_name')
-        if value is not None and name is not None and currency is not None:
-            amount = Price(value, currency=currency)
-            return FixedDiscount(amount, name)
+        if value is not None and currency is not None:
+            return Money(value, currency=currency)
         return None
 
-    @discount.setter
-    def discount(self, discount):
-        amount = discount.amount
-        self.storage['discount_value'] = smart_text(amount.net)
-        self.storage['discount_currency'] = amount.currency
-        self.storage['discount_name'] = discount.name
+    @discount_amount.setter
+    def discount_amount(self, discount):
+        self.storage['discount_value'] = smart_text(discount.amount)
+        self.storage['discount_currency'] = discount.currency
         self.modified = True
 
-    @discount.deleter
-    def discount(self):
+    @discount_amount.deleter
+    def discount_amount(self):
         if 'discount_value' in self.storage:
             del self.storage['discount_value']
             self.modified = True
         if 'discount_currency' in self.storage:
             del self.storage['discount_currency']
             self.modified = True
+
+    @property
+    def discount_name(self):
+        """Return a discount name if any."""
+        return self.storage.get('discount_name')
+
+    @discount_name.setter
+    def discount_name(self, discount_name):
+        self.storage['discount_name'] = discount_name
+        self.modified = True
+
+    @discount_name.deleter
+    def discount_name(self):
         if 'discount_name' in self.storage:
             del self.storage['discount_name']
             self.modified = True
@@ -305,15 +316,19 @@ class Checkout:
         self._add_to_user_address_book(
             self.billing_address, is_billing=True)
 
-        shipping_price = (
-            self.shipping_method.get_total() if self.shipping_method
-            else Price(0, currency=settings.DEFAULT_CURRENCY))
+        if self.shipping_method:
+            shipping_price = self.shipping_method.get_total()
+        else:
+            shipping_price = TaxedMoney(
+                net=Money(0, currency=settings.DEFAULT_CURRENCY),
+                gross=Money(0, currency=settings.DEFAULT_CURRENCY))
+
         order_data = {
             'language_code': get_language(),
             'billing_address': billing_address,
             'shipping_address': shipping_address,
             'tracking_client_id': self.tracking_code,
-            'shipping_price': shipping_price,
+            'shipping_price': shipping_price.gross,
             'total': self.get_total()}
 
         if self.user.is_authenticated:
@@ -323,10 +338,9 @@ class Checkout:
             order_data['user_email'] = self.email
 
         if voucher is not None:
-            discount = self.discount
             order_data['voucher'] = voucher
-            order_data['discount_amount'] = discount.amount
-            order_data['discount_name'] = discount.name
+            order_data['discount_amount'] = self.discount_amount
+            order_data['discount_name'] = self.discount_name
 
         order = Order.objects.create(**order_data)
 
@@ -368,18 +382,24 @@ class Checkout:
         voucher = self._get_voucher()
         if voucher is not None:
             try:
-                self.discount = get_voucher_discount_for_checkout(
+                self.discount_amount = get_voucher_discount_for_checkout(
                     voucher, self)
+                self.discount_name = voucher.name
             except NotApplicable:
-                del self.discount
+                del self.discount_amount
+                del self.discount_name
                 del self.voucher_code
         else:
-            del self.discount
+            del self.discount_amount
+            del self.discount_name
             del self.voucher_code
 
     def get_subtotal(self):
-        """Calculate order total without shipping."""
-        zero = Price(0, currency=settings.DEFAULT_CURRENCY)
+        """Calculate order total without shipping and discount."""
+        zero = TaxedMoney(
+            net=Money(0, currency=settings.DEFAULT_CURRENCY),
+            gross=Money(0, currency=settings.DEFAULT_CURRENCY))
+
         cost_iterator = (
             total - shipping_cost
             for shipment, shipping_cost, total in self.deliveries)
@@ -387,13 +407,18 @@ class Checkout:
         return total
 
     def get_total(self):
-        """Calculate order total with shipping."""
-        zero = Price(0, currency=settings.DEFAULT_CURRENCY)
+        """Calculate order total with shipping and discount amount."""
+        zero = TaxedMoney(
+            net=Money(0, currency=settings.DEFAULT_CURRENCY),
+            gross=Money(0, currency=settings.DEFAULT_CURRENCY))
+
         cost_iterator = (
             total
             for shipment, shipping_cost, total in self.deliveries)
         total = sum(cost_iterator, zero)
-        return total if self.discount is None else self.discount.apply(total)
+        if self.discount_amount:
+            return total - self.discount_amount
+        return total
 
 
 def load_checkout(view):

saleor/discount/utils.py
@@ -2,8 +2,7 @@ from django.conf import settings
 from django.db.models import F
 from django.utils.encoding import smart_text
 from django.utils.translation import pgettext
-from prices import FixedDiscount, Price
-
+from prices import Money, TaxedMoney
 
 from ..cart.utils import (
     get_category_variants_and_prices, get_product_variants_and_prices)
@@ -57,10 +56,9 @@ def get_product_discounts(product, discounts):
 def calculate_discounted_price(product, price, discounts):
     """Return minimum product's price of all prices with discounts applied."""
     if discounts:
-        discounts = list(
-            get_product_discounts(product, discounts))
+        discounts = list(get_product_discounts(product, discounts))
         if discounts:
-            price = min(price | discount for discount in discounts)
+            price = min(discount(price) for discount in discounts)
     return price
 
 
@@ -68,7 +66,7 @@ def _get_value_voucher_discount_for_checkout(voucher, checkout):
     """Calculate discount value for a voucher of value type."""
     cart_total = checkout.get_subtotal()
     voucher.validate_limit(cart_total)
-    return voucher.get_fixed_discount_for(cart_total)
+    return voucher.get_discount_amount_for(cart_total)
 
 
 def _get_shipping_voucher_discount_for_checkout(voucher, checkout):
@@ -94,7 +92,7 @@ def _get_shipping_voucher_discount_for_checkout(voucher, checkout):
             msg % {'country': voucher.get_apply_to_display()})
     cart_total = checkout.get_subtotal()
     voucher.validate_limit(cart_total)
-    return voucher.get_fixed_discount_for(shipping_method.price)
+    return voucher.get_discount_amount_for(shipping_method.get_total())
 
 
 def _get_product_or_category_voucher_discount_for_checkout(voucher, checkout):
@@ -114,14 +112,15 @@ def _get_product_or_category_voucher_discount_for_checkout(voucher, checkout):
         raise NotApplicable(msg)
     if voucher.apply_to == VoucherApplyToProduct.ALL_PRODUCTS:
         discounts = (
-            voucher.get_fixed_discount_for(price) for price in prices)
-        discount_total = sum(
-            (discount.amount for discount in discounts),
-            Price(0, currency=settings.DEFAULT_CURRENCY))
-        return FixedDiscount(discount_total, smart_text(voucher))
-    product_total = sum(
-        prices, Price(0, currency=settings.DEFAULT_CURRENCY))
-    return voucher.get_fixed_discount_for(product_total)
+            voucher.get_discount_amount_for(price) for price in prices)
+        total_amount = sum(
+            discounts, Money(0, currency=settings.DEFAULT_CURRENCY))
+        return total_amount
+    empty_total = TaxedMoney(
+        net=Money(0, currency=settings.DEFAULT_CURRENCY),
+        gross=Money(0, currency=settings.DEFAULT_CURRENCY))
+    product_total = sum(prices, empty_total)
+    return voucher.get_discount_amount_for(product_total)
 
 
 def get_voucher_discount_for_checkout(voucher, checkout):

tests/test_checkout.py
@@ -3,7 +3,7 @@ from unittest.mock import MagicMock, Mock
 import pytest
 from django.conf import settings
 from django.contrib.auth.models import AnonymousUser
-from prices import Price
+from prices import Money, TaxedMoney
 
 from saleor.account.models import Address
 from saleor.checkout import views
@@ -49,38 +49,34 @@ def test_checkout_is_shipping_required():
 
 
 def test_checkout_deliveries():
+    item_price = TaxedMoney(net=Money(10, 'USD'), gross=Money(10, 'USD'))
     partition = Mock(
-        get_total=Mock(
-            return_value=Price(10, currency=settings.DEFAULT_CURRENCY)),
-        get_price_per_item=Mock(
-            return_value=Price(10, currency=settings.DEFAULT_CURRENCY)))
+        get_total=Mock(return_value=item_price),
+        get_price_per_item=Mock(return_value=item_price))
 
     def f():
         yield partition
 
     partition.__iter__ = Mock(return_value=f())
-    cart = Mock(partition=Mock(return_value=[partition]),
-                currency=settings.DEFAULT_CURRENCY)
-    checkout = Checkout(
-        cart, AnonymousUser(), 'tracking_code')
+    cart = Mock(partition=Mock(return_value=[partition]), currency='USD')
+    checkout = Checkout(cart, AnonymousUser(), 'tracking_code')
     deliveries = list(checkout.deliveries)
-    assert deliveries[0][1] == Price(0, currency=settings.DEFAULT_CURRENCY)
+    assert deliveries[0][1] == TaxedMoney(
+        net=Money(0, 'USD'), gross=Money(0, 'USD'))
     assert deliveries[0][2] == partition.get_total()
     assert deliveries[0][0][0][0] == partition
 
 
 def test_checkout_deliveries_with_shipping_method(monkeypatch):
-    shipping_cost = 5
-    items_cost = 5
+    shipping_cost = Money(3, currency=settings.DEFAULT_CURRENCY)
+    items_cost = Money(5, currency=settings.DEFAULT_CURRENCY)
+    cost_with_shipping = items_cost + shipping_cost
 
+    items_price = TaxedMoney(net=items_cost, gross=items_cost)
     partition = Mock(
         is_shipping_required=MagicMock(return_value=True),
-        get_total=Mock(
-            return_value=Price(
-                items_cost, currency=settings.DEFAULT_CURRENCY)),
-        get_price_per_item=Mock(
-            return_value=Price(
-                items_cost, currency=settings.DEFAULT_CURRENCY)))
+        get_total=Mock(return_value=items_price),
+        get_price_per_item=Mock(return_value=items_price))
 
     def f():
         yield partition
@@ -90,20 +86,16 @@ def test_checkout_deliveries_with_shipping_method(monkeypatch):
         partition=Mock(return_value=[partition]),
         currency=settings.DEFAULT_CURRENCY)
 
-    shipping_method_mock = Mock(
-        get_total=Mock(
-            return_value=Price(
-                shipping_cost, currency=settings.DEFAULT_CURRENCY)))
+    shipping_price = TaxedMoney(net=shipping_cost, gross=shipping_cost)
+    shipping_method_mock = Mock(get_total=Mock(return_value=shipping_price))
     monkeypatch.setattr(Checkout, 'shipping_method', shipping_method_mock)
 
-    checkout = Checkout(
-        cart, AnonymousUser(), 'tracking_code')
-
+    checkout = Checkout(cart, AnonymousUser(), 'tracking_code')
     deliveries = list(checkout.deliveries)
-    assert deliveries[0][1] == Price(
-        shipping_cost, currency=settings.DEFAULT_CURRENCY)
-    assert deliveries[0][2] == Price(
-        items_cost + shipping_cost, currency=settings.DEFAULT_CURRENCY)
+
+    assert deliveries[0][1] == shipping_price
+    assert deliveries[0][2] == TaxedMoney(
+        net=cost_with_shipping, gross=cost_with_shipping)
     assert deliveries[0][0][0][0] == partition
 
 
@@ -239,7 +231,8 @@ def test_checkout_discount(request_cart, sale, product_in_stock):
     variant = product_in_stock.variants.get()
     request_cart.add(variant, 1)
     checkout = Checkout(request_cart, AnonymousUser(), 'tracking_code')
-    assert checkout.get_total() == Price(currency="USD", net=5)
+    assert checkout.get_total() == TaxedMoney(
+        net=Money(5, currency="USD"), gross=Money(5, currency="USD"))
 
 
 def test_checkout_create_order_insufficient_stock(

Sajmon-Whys/saleor

saleor/checkout/core.py
@@ -7,7 +7,7 @@ from django.db import transaction
 from django.forms.models import model_to_dict
 from django.utils.encoding import smart_text
 from django.utils.translation import get_language
-from prices import FixedDiscount, Price
+from prices import Money, TaxedMoney
 
 from ..account.models import Address
 from ..account.utils import store_user_address
@@ -106,9 +106,11 @@ class Checkout:
         """
         for partition in self.cart.partition():
             if self.shipping_method and partition.is_shipping_required():
-                shipping_cost = self.shipping_method.get_total()
+                shipping_cost = self.shipping_method.get_gross_price()
             else:
-                shipping_cost = Price(0, currency=settings.DEFAULT_CURRENCY)
+                shipping_cost = TaxedMoney(
+                    net=Money(0, currency=settings.DEFAULT_CURRENCY),
+                    gross=Money(0, currency=settings.DEFAULT_CURRENCY))
             total_with_shipping = partition.get_total(
                 discounts=self.cart.discounts) + shipping_cost
 
@@ -210,32 +212,41 @@ class Checkout:
         self.modified = True
 
     @property
-    def discount(self):
-        """Return a discount if any."""
+    def discount_amount(self):
+        """Return a discount amount if any."""
         value = self.storage.get('discount_value')
         currency = self.storage.get('discount_currency')
-        name = self.storage.get('discount_name')
-        if value is not None and name is not None and currency is not None:
-            amount = Price(value, currency=currency)
-            return FixedDiscount(amount, name)
+        if value is not None and currency is not None:
+            return Money(value, currency=currency)
         return None
 
-    @discount.setter
-    def discount(self, discount):
-        amount = discount.amount
-        self.storage['discount_value'] = smart_text(amount.net)
-        self.storage['discount_currency'] = amount.currency
-        self.storage['discount_name'] = discount.name
+    @discount_amount.setter
+    def discount_amount(self, discount):
+        self.storage['discount_value'] = smart_text(discount.amount)
+        self.storage['discount_currency'] = discount.currency
         self.modified = True
 
-    @discount.deleter
-    def discount(self):
+    @discount_amount.deleter
+    def discount_amount(self):
         if 'discount_value' in self.storage:
             del self.storage['discount_value']
             self.modified = True
         if 'discount_currency' in self.storage:
             del self.storage['discount_currency']
             self.modified = True
+
+    @property
+    def discount_name(self):
+        """Return a discount name if any."""
+        return self.storage.get('discount_name')
+
+    @discount_name.setter
+    def discount_name(self, discount_name):
+        self.storage['discount_name'] = discount_name
+        self.modified = True
+
+    @discount_name.deleter
+    def discount_name(self):
         if 'discount_name' in self.storage:
             del self.storage['discount_name']
             self.modified = True
@@ -305,15 +316,19 @@ class Checkout:
         self._add_to_user_address_book(
             self.billing_address, is_billing=True)
 
-        shipping_price = (
-            self.shipping_method.get_total() if self.shipping_method
-            else Price(0, currency=settings.DEFAULT_CURRENCY))
+        if self.shipping_method:
+            shipping_price = self.shipping_method.get_gross_price()
+        else:
+            shipping_price = TaxedMoney(
+                net=Money(0, currency=settings.DEFAULT_CURRENCY),
+                gross=Money(0, currency=settings.DEFAULT_CURRENCY))
+
         order_data = {
             'language_code': get_language(),
             'billing_address': billing_address,
             'shipping_address': shipping_address,
             'tracking_client_id': self.tracking_code,
-            'shipping_price': shipping_price,
+            'shipping_price': shipping_price.gross,
             'total': self.get_total()}
 
         if self.user.is_authenticated:
@@ -323,10 +338,9 @@ class Checkout:
             order_data['user_email'] = self.email
 
         if voucher is not None:
-            discount = self.discount
             order_data['voucher'] = voucher
-            order_data['discount_amount'] = discount.amount
-            order_data['discount_name'] = discount.name
+            order_data['discount_amount'] = self.discount_amount
+            order_data['discount_name'] = self.discount_name
 
         order = Order.objects.create(**order_data)
 
@@ -368,32 +382,43 @@ class Checkout:
         voucher = self._get_voucher()
         if voucher is not None:
             try:
-                self.discount = get_voucher_discount_for_checkout(
+                self.discount_amount = get_voucher_discount_for_checkout(
                     voucher, self)
+                self.discount_name = voucher.name
             except NotApplicable:
-                del self.discount
+                del self.discount_amount
+                del self.discount_name
                 del self.voucher_code
         else:
-            del self.discount
+            del self.discount_amount
+            del self.discount_name
             del self.voucher_code
 
     def get_subtotal(self):
-        """Calculate order total without shipping."""
-        zero = Price(0, currency=settings.DEFAULT_CURRENCY)
+        """Calculate order total without shipping and discount."""
+        zero_subtotal = TaxedMoney(
+            net=Money(0, currency=settings.DEFAULT_CURRENCY),
+            gross=Money(0, currency=settings.DEFAULT_CURRENCY))
+
         cost_iterator = (
             total - shipping_cost
             for shipment, shipping_cost, total in self.deliveries)
-        total = sum(cost_iterator, zero)
+        total = sum(cost_iterator, zero_subtotal)
         return total
 
     def get_total(self):
-        """Calculate order total with shipping."""
-        zero = Price(0, currency=settings.DEFAULT_CURRENCY)
+        """Calculate order total with shipping and discount amount."""
+        zero_total = TaxedMoney(
+            net=Money(0, currency=settings.DEFAULT_CURRENCY),
+            gross=Money(0, currency=settings.DEFAULT_CURRENCY))
+
         cost_iterator = (
             total
             for shipment, shipping_cost, total in self.deliveries)
-        total = sum(cost_iterator, zero)
-        return total if self.discount is None else self.discount.apply(total)
+        total = sum(cost_iterator, zero_total)
+        if self.discount_amount:
+            return total - self.discount_amount
+        return total
 
 
 def load_checkout(view):

saleor/discount/utils.py
@@ -2,8 +2,7 @@ from django.conf import settings
 from django.db.models import F
 from django.utils.encoding import smart_text
 from django.utils.translation import pgettext
-from prices import FixedDiscount, Price
-
+from prices import Money, TaxedMoney
 
 from ..cart.utils import (
     get_category_variants_and_prices, get_product_variants_and_prices)
@@ -57,10 +56,9 @@ def get_product_discounts(product, discounts):
 def calculate_discounted_price(product, price, discounts):
     """Return minimum product's price of all prices with discounts applied."""
     if discounts:
-        discounts = list(
-            get_product_discounts(product, discounts))
+        discounts = list(get_product_discounts(product, discounts))
         if discounts:
-            price = min(price | discount for discount in discounts)
+            price = min(discount(price) for discount in discounts)
     return price
 
 
@@ -68,7 +66,7 @@ def _get_value_voucher_discount_for_checkout(voucher, checkout):
     """Calculate discount value for a voucher of value type."""
     cart_total = checkout.get_subtotal()
     voucher.validate_limit(cart_total)
-    return voucher.get_fixed_discount_for(cart_total)
+    return voucher.get_discount_amount_for(cart_total)
 
 
 def _get_shipping_voucher_discount_for_checkout(voucher, checkout):
@@ -94,7 +92,7 @@ def _get_shipping_voucher_discount_for_checkout(voucher, checkout):
             msg % {'country': voucher.get_apply_to_display()})
     cart_total = checkout.get_subtotal()
     voucher.validate_limit(cart_total)
-    return voucher.get_fixed_discount_for(shipping_method.price)
+    return voucher.get_discount_amount_for(shipping_method.get_total())
 
 
 def _get_product_or_category_voucher_discount_for_checkout(voucher, checkout):
@@ -114,14 +112,15 @@ def _get_product_or_category_voucher_discount_for_checkout(voucher, checkout):
         raise NotApplicable(msg)
     if voucher.apply_to == VoucherApplyToProduct.ALL_PRODUCTS:
         discounts = (
-            voucher.get_fixed_discount_for(price) for price in prices)
-        discount_total = sum(
-            (discount.amount for discount in discounts),
-            Price(0, currency=settings.DEFAULT_CURRENCY))
-        return FixedDiscount(discount_total, smart_text(voucher))
-    product_total = sum(
-        prices, Price(0, currency=settings.DEFAULT_CURRENCY))
-    return voucher.get_fixed_discount_for(product_total)
+            voucher.get_discount_amount_for(price) for price in prices)
+        total_amount = sum(
+            discounts, Money(0, currency=settings.DEFAULT_CURRENCY))
+        return total_amount
+    zero_total = TaxedMoney(
+        net=Money(0, currency=settings.DEFAULT_CURRENCY),
+        gross=Money(0, currency=settings.DEFAULT_CURRENCY))
+    product_total = sum(prices, zero_total)
+    return voucher.get_discount_amount_for(product_total)
 
 
 def get_voucher_discount_for_checkout(voucher, checkout):

tests/test_checkout.py
@@ -3,7 +3,7 @@ from unittest.mock import MagicMock, Mock
 import pytest
 from django.conf import settings
 from django.contrib.auth.models import AnonymousUser
-from prices import Price
+from prices import Money, TaxedMoney
 
 from saleor.account.models import Address
 from saleor.checkout import views
@@ -49,38 +49,34 @@ def test_checkout_is_shipping_required():
 
 
 def test_checkout_deliveries():
+    item_price = TaxedMoney(net=Money(10, 'USD'), gross=Money(10, 'USD'))
     partition = Mock(
-        get_total=Mock(
-            return_value=Price(10, currency=settings.DEFAULT_CURRENCY)),
-        get_price_per_item=Mock(
-            return_value=Price(10, currency=settings.DEFAULT_CURRENCY)))
+        get_total=Mock(return_value=item_price),
+        get_price_per_item=Mock(return_value=item_price))
 
     def f():
         yield partition
 
     partition.__iter__ = Mock(return_value=f())
-    cart = Mock(partition=Mock(return_value=[partition]),
-                currency=settings.DEFAULT_CURRENCY)
-    checkout = Checkout(
-        cart, AnonymousUser(), 'tracking_code')
+    cart = Mock(partition=Mock(return_value=[partition]), currency='USD')
+    checkout = Checkout(cart, AnonymousUser(), 'tracking_code')
     deliveries = list(checkout.deliveries)
-    assert deliveries[0][1] == Price(0, currency=settings.DEFAULT_CURRENCY)
+    assert deliveries[0][1] == TaxedMoney(
+        net=Money(0, 'USD'), gross=Money(0, 'USD'))
     assert deliveries[0][2] == partition.get_total()
     assert deliveries[0][0][0][0] == partition
 
 
 def test_checkout_deliveries_with_shipping_method(monkeypatch):
-    shipping_cost = 5
-    items_cost = 5
+    shipping_cost = Money(3, currency=settings.DEFAULT_CURRENCY)
+    items_cost = Money(5, currency=settings.DEFAULT_CURRENCY)
+    cost_with_shipping = items_cost + shipping_cost
 
+    items_price = TaxedMoney(net=items_cost, gross=items_cost)
     partition = Mock(
         is_shipping_required=MagicMock(return_value=True),
-        get_total=Mock(
-            return_value=Price(
-                items_cost, currency=settings.DEFAULT_CURRENCY)),
-        get_price_per_item=Mock(
-            return_value=Price(
-                items_cost, currency=settings.DEFAULT_CURRENCY)))
+        get_total=Mock(return_value=items_price),
+        get_price_per_item=Mock(return_value=items_price))
 
     def f():
         yield partition
@@ -90,20 +86,16 @@ def test_checkout_deliveries_with_shipping_method(monkeypatch):
         partition=Mock(return_value=[partition]),
         currency=settings.DEFAULT_CURRENCY)
 
-    shipping_method_mock = Mock(
-        get_total=Mock(
-            return_value=Price(
-                shipping_cost, currency=settings.DEFAULT_CURRENCY)))
+    shipping_price = TaxedMoney(net=shipping_cost, gross=shipping_cost)
+    shipping_method_mock = Mock(get_gross_price=Mock(return_value=shipping_price))
     monkeypatch.setattr(Checkout, 'shipping_method', shipping_method_mock)
 
-    checkout = Checkout(
-        cart, AnonymousUser(), 'tracking_code')
-
+    checkout = Checkout(cart, AnonymousUser(), 'tracking_code')
     deliveries = list(checkout.deliveries)
-    assert deliveries[0][1] == Price(
-        shipping_cost, currency=settings.DEFAULT_CURRENCY)
-    assert deliveries[0][2] == Price(
-        items_cost + shipping_cost, currency=settings.DEFAULT_CURRENCY)
+
+    assert deliveries[0][1] == shipping_price
+    assert deliveries[0][2] == TaxedMoney(
+        net=cost_with_shipping, gross=cost_with_shipping)
     assert deliveries[0][0][0][0] == partition
 
 
@@ -239,7 +231,8 @@ def test_checkout_discount(request_cart, sale, product_in_stock):
     variant = product_in_stock.variants.get()
     request_cart.add(variant, 1)
     checkout = Checkout(request_cart, AnonymousUser(), 'tracking_code')
-    assert checkout.get_total() == Price(currency="USD", net=5)
+    assert checkout.get_total() == TaxedMoney(
+        net=Money(5, currency="USD"), gross=Money(5, currency="USD"))
 
 
 def test_checkout_create_order_insufficient_stock(